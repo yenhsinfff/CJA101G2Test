ed2ad6cc301488278900ef3b6aa6a11a406f886f
soccerchou <pingfeng24@gmail.com>
2025-05-15 16:22:19 +0800

    third commit

--- src/main/java/com/lutu/camp/CampVO.java
+++ src/main/java/com/lutu/camp/CampVO.java
@@ -2,4 +2,15 @@ package com.lutu.camp;
 
 public class CampVO {
 
+    private String name;
+    private int max;
+    private int min;
+    private int current;
+    public CampVO(String name, int max, int min, int current) {
+        this.name = name;
+        this.max = max;
+        this.min = min;
+        this.current = current;
+    }
+
 }
